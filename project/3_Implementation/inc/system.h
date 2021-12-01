10eee4eb8d0882f0cc25aff008caffa30ac048e7
padma510 <94212251+padma510@users.noreply.github.com>
2021-12-01 19:46:21 +0530

    Update system.h

--- project/3_Implementation/inc/system.h
+++ project/3_Implementation/inc/system.h
@@ -1,53 +1,27 @@
 #include<avr/io.h>
-
 #include<util/delay.h>
-
 #include<uart.h>
-
 #include<lcd.h>
-
- 
-
 void read_finger_1()          //for char_buffer1
-
 {
-
       int i=0;
-
       char k=1,ch=1;
 
- 
-
       sendchar_uart(239);
-
       sendchar_uart(1);
-
       sendchar_uart(255);
-
       sendchar_uart(255);
-
       sendchar_uart(255);
-
       sendchar_uart(255);
-
       sendchar_uart(1);
-
       sendchar_uart(0);
-
       sendchar_uart(3);
-
       sendchar_uart(1);
-
       sendchar_uart(0);
-
       sendchar_uart(5);
 
- 
-
       for(i=0;i<10;i++)
-
       {
-
             k=getchar_uart();
 
             if(i==9)
