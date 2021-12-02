19b5df645d767952b92cc54f6576d8abee653e20
padma510 <94212251+padma510@users.noreply.github.com>
2021-12-02 15:39:43 +0530

    Update system.h

--- project/3_Implementation/inc/system.h
+++ project/3_Implementation/inc/system.h
@@ -1,109 +1,15 @@
-#include<avr/io.h>
-#include<util/delay.h>
-#include<uart.h>
-#include<lcd.h>
-void read_finger_1()          //for char_buffer1
-{
-      int i=0;
-      char k=1,ch=1;
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <TFT_ILI9163C.h>
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
 
-      sendchar_uart(239);
-      sendchar_uart(1);
-      sendchar_uart(255);
-      sendchar_uart(255);
-      sendchar_uart(255);
-      sendchar_uart(255);
-      sendchar_uart(1);
-      sendchar_uart(0);
-      sendchar_uart(3);
-      sendchar_uart(1);
-      sendchar_uart(0);
-      sendchar_uart(5);
-
-      for(i=0;i<10;i++)
-      {
-            k=getchar_uart();
-
-            if(i==9)
-
-            {
-
-                  ch=k;
-
-                  k=getchar_uart();
-
-                  k=getchar_uart();
-
-                  if(ch==0x00)
-
-                  {
-
-                        PORTC|=(1<<0);
-
-                        k=1;
-
-                        sendchar_uart(239);
-
-                        sendchar_uart(1);
-
-                        sendchar_uart(255);
-
-                        sendchar_uart(255);
-
-                        sendchar_uart(255);
-
-                        sendchar_uart(255);
-
-                        sendchar_uart(1);
-
-                        sendchar_uart(0);
-
-                        sendchar_uart(4);
-
-                        sendchar_uart(2);
-
-                        sendchar_uart(1);
-
-                        sendchar_uart(0);
-
-                        sendchar_uart(8);
-
-                        i=0;
-
-                        for(i=0;i<10;i++)
-
-                        {
-
-                              k=getchar_uart();
-
-                              if(i==9)
-
-                              {
-
-                                    ch=k;
-
-                                    k=getchar_uart();
-
-                                    k=getchar_uart();
-
-                                    if(ch==0x00)
-
-                                    {
-
-                                          PORTC|=(1<<1);
-
-                                    }
-
-                              }
-
-                        }
-
-                  }
-
-            }
-
-      }
-
-}
-
- 
+// Color definitions
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0  
+#define WHITE   0xFFFF
