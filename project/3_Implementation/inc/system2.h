8dbc07d63bc14a82f5b1432ffce75e1b0ba54b29
padma510 <94212251+padma510@users.noreply.github.com>
2021-12-02 15:40:39 +0530

    Update system2.h

--- project/3_Implementation/inc/system2.h
+++ project/3_Implementation/inc/system2.h
@@ -1,26 +1,21 @@
-#include<avr/io.h>
-#include<util/delay.h>
-#include<uart.h>
-#include<lcd.h>
-void main()
-{
-      DDRC=0xff;
-      PORTC=0;
+void setup(void) {
 
-      enable_uart(9600);
-      LCDinit();
-      LCDclr();
-      _delay_ms(5000);  //plenty of delay for modules initialization
-      LCDdisplay("Scanning.....");
-      read_finger_1();  //scans and stores in char_buffer1
-      read_finger_2();  //scans and stores in char_buffer2
-      make_template();  //makes the template with info in char_buffer1 & char_buffer2 and stores it in char_buffer
-      check_finger();         //checks for the finger authentication
-//    store(0);               //stores the scanned value to the given parametric location in flash library
-//    empty();                //empties the flash library
-
+  startFingerprintSensor();
+  display.begin();
+  displayLockScreen(); 
 }
 
- 
+void loop() {
 
-   
+  fingerprintID = getFingerprintID();
+  delay(50);
+  if(fingerprintID == 1)
+  {
+    display.drawBitmap(30,35,icon,60,60,GREEN);
+    delay(2000);
+    displayUnlockedScreen();
+    displayIoanna();
+    delay(5000);
+    display.fillScreen(BLACK);
+    displayLockScreen();
+
